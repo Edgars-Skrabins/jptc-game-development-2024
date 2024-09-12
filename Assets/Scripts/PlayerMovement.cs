520ceb9b2faea158acee6bd8012cbb7d622eadec
Edgars Skrabiņš <shiftkiro@gmail.com>
2024-09-12 20:27:35 +0300

    Added translate code + debug log code

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // [SerializeField] private string m_myText = "Hello world!";
+
+    // private void Start()
+    // {
+    //     LogMyText();
+    // }
+    //
+    // private void LogMyText()
+    // {
+    //     Debug.Log(m_myText);
+    // }
+
+    [SerializeField] private float m_speed;
+
+    private void Update()
+    {
+        Vector3 forwardVector = new Vector3(0.0f, 0.0f, 1f); // Can be replaced by Vector3.forward
+        transform.Translate(forwardVector * (m_speed * Time.deltaTime));
+    }
+}
